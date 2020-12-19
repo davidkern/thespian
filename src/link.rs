1b88860732cc05186de1ad66d15734355d4afb6d
David Kern <david@mju.io>
2020-12-18 19:29:19 -0800

    Test link

--- src/link.rs
+++ src/link.rs
@@ -6,6 +6,12 @@ use tokio::sync::mpsc;
 
 pub struct Sender<T>(mpsc::Sender<T>);
 
+impl<T> From<mpsc::Sender<T>> for Sender<T> {
+    fn from(sender: mpsc::Sender<T>) -> Self {
+        Self(sender)
+    }
+}
+
 impl<T> Sender<T> {
     pub async fn send(&self, value: T) {
         match self.0.send(value).await {
@@ -28,5 +34,41 @@ pub struct Receiver<T>(mpsc::Receiver<T>);
 impl<T> Receiver<T> {
     pub async fn recv(&mut self) -> Option<T> {
         self.0.recv().await
+    }
+}
+
+impl<T> From<mpsc::Receiver<T>> for Receiver<T> {
+    fn from(receiver: mpsc::Receiver<T>) -> Self {
+        Self(receiver)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[derive(PartialEq, Debug)]
+    enum Msg {
+        A, B
+    }
+
+    #[tokio::test]
+    async fn send_and_receive() {
+        let (tx, rx) = mpsc::channel(1);
+        let sender = Sender::from(tx);
+        let mut receiver = Receiver::from(rx);
+
+        let (_sent, _received) = tokio::join! {
+            // send Msg::A from sender and Msg::B from a clone
+            async {
+                sender.send(Msg::A).await;
+                sender.clone().send(Msg::B).await;
+            },
+
+            async {
+                assert_eq!(Msg::A, receiver.recv().await.unwrap());
+                assert_eq!(Msg::B, receiver.recv().await.unwrap());
+            }
+        };
     }
 }
