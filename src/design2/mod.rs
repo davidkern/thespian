7ac08cf71988e59f8dfbd305bd3b8bf088ddf658
David Kern <david@mju.io>
2020-12-27 13:06:57 -0800

    Start adding exit reason and process link

--- src/design2/mod.rs
+++ src/design2/mod.rs
@@ -8,18 +8,25 @@ mod test {
     pub enum Foo {
         A,
         B,
+        Stop,
     }
     
-    impl Process for Foo {
+    impl Actor for Foo {
         fn spawn() -> PendingPid<Self> {
-            Stage::spawn(|msg: Self| async move {
+            Stage::spawn(|msg| async move {
                 match msg {
                     Self::A => {
                         println!("A!");
+                        Exit::Continue
                     },
                     Self::B => {
                         println!("B!");
-                    }
+                        Exit::Continue
+                    },
+                    Self::Stop => {
+                        println!("Stop!");
+                        Exit::Normal
+                    },
                 }
             })
         }
@@ -27,66 +34,104 @@ mod test {
     
     #[tokio::test]
     async fn example() {
+        println!("test start");
         let pid = Foo::spawn().pid().await;
-        pid.send(Foo::A).await;
-        pid.send(Foo::B).await;
+        println!("received pid");
 
-        // This send will never be processed, but forces the previous
-        // message to be processed.  Program exits before the process
-        // receives this one, and can't fix that until there is a way
-        // to await process exit.
+        pid.send(Foo::A).await;
+        println!("sent A");
         pid.send(Foo::B).await;
-    }    
+        println!("sent B");        
+    }
 }
 
 // Implementation
 
-pub trait Process: Sized {
+pub trait Actor: Sized {
     fn spawn() -> PendingPid<Self>;
+}
+
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub enum Exit {
+    Continue,       // Continue to run
+    Normal,         // Exit normally, linked processes continue to run
+    Error,          // Exit with error condition, terminate linked processes
+}
+
+pub enum Msg<P> {
+    LinkExit(Exit),
+    Actor(P),
 }
 
 pub struct Stage;
 
 impl Stage {
-    pub fn spawn<P: Process, F, Fut>(mut f: F) -> PendingPid<P>
+    pub fn spawn<P: Actor, F, Fut>(mut f: F) -> PendingPid<P>
     where
         P: Send + 'static,
         F: FnMut(P) -> Fut + Send + 'static,
-        Fut: Future<Output=()> + Send + 'static
+        Fut: Future<Output=Exit> + Send + 'static
     {
         let (pid_sender, pid_receiver) = tokio::sync::oneshot::channel::<Pid<P>>();
 
         tokio::spawn(async move {
-            let (sender, mut receiver) = tokio::sync::mpsc::channel::<P>(1);
+            println!("running async block");
+
+            // Create a pid with a message sender and a process-exit sender
+            let (sender, mut receiver) = tokio::sync::mpsc::channel::<Msg<P>>(1);
+            let (exit_sender, exit_receiver) = tokio::sync::oneshot::channel::<Exit>();
             pid_sender.send(Pid{ sender }).ok();
 
+            println!("sent pid");
+
             while let Some(msg) = receiver.recv().await {
-                f(msg).await;
+                println!("received msg");
+
+                match msg {
+                    Msg::Actor(actor_msg) => {
+                        let exit = f(actor_msg).await;
+                        if exit != Exit::Continue {
+                            println!("sending exit");
+                            exit_sender.send(exit).ok();
+                            break;
+                        }        
+                    },
+                    Msg::LinkExit(exit) => {
+                        println!("received exit {:?} from link", exit);
+                        if exit == Exit::Error {
+                            println!("sending exit received from link");
+                            exit_sender.send(exit).ok();
+                            break;
+                        } else {
+                            println!("ignoring exit received from link");
+                        }
+                    },
+                }
             }
 
-            println!("exiting");
+            println!("async block exiting");
         });
 
         PendingPid{ receiver: pid_receiver }
     }
 }
 
-pub struct PendingPid<P: Process> {
+pub struct PendingPid<P: Actor> {
     receiver: tokio::sync::oneshot::Receiver<Pid<P>>,
 }
 
-impl<P: Process> PendingPid<P> {
+impl<P: Actor> PendingPid<P> {
     pub async fn pid(self) -> Pid<P> {
         self.receiver.await.unwrap()
     }
 }
 
-pub struct Pid<P: Process> {
-    sender: tokio::sync::mpsc::Sender<P>,
+pub struct Pid<P: Actor> {
+    sender: tokio::sync::mpsc::Sender<Msg<P>>,
 }
 
-impl<P: Process> Pid<P> {
+impl<P: Actor> Pid<P> {
     pub async fn send(&self, msg: P) {
-        self.sender.send(msg).await.ok();
+        self.sender.send(Msg::Actor(msg)).await.ok();
     }
 }
