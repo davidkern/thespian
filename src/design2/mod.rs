bbf2f4f963d06b11ba9bd2103fca5cf65f80c89d
David Kern <david@mju.io>
2020-12-28 23:09:56 -0800

    Typed/untyped pids and other fun

--- src/design2/mod.rs
+++ src/design2/mod.rs
@@ -11,8 +11,8 @@ mod test {
         Stop,
     }
     
-    impl Actor for Foo {
-        fn spawn() -> PendingPid<Self> {
+    impl Process for Foo {
+        fn spawn() -> PendingHandle<Self> {
             Stage::spawn(|msg| async move {
                 match msg {
                     Self::A => {
@@ -47,8 +47,8 @@ mod test {
 
 // Implementation
 
-pub trait Actor: Sized {
-    fn spawn() -> PendingPid<Self>;
+pub trait Process: Sized {
+    fn spawn() -> PendingHandle<Self>;
 }
 
 #[derive(PartialEq, Copy, Clone, Debug)]
@@ -58,15 +58,23 @@ pub enum Exit {
     Error,          // Exit with error condition, terminate linked processes
 }
 
-pub enum Msg<P> {
+/// Messages handled internally for all processes
+pub enum SystemMsg {
+    Link(UntypedPid),
+    Unlink(UntypedPid),
     LinkExit(Exit),
-    Actor(P),
+}
+
+/// Messages handled by the process loop inside of spawn
+pub enum ProcessMsg<P> {
+    System(SystemMsg),      // system implemented message
+    Process(P),             // process implemented message
 }
 
 pub struct Stage;
 
 impl Stage {
-    pub fn spawn<P: Actor, F, Fut>(mut f: F) -> PendingPid<P>
+    pub fn spawn<P: Process, F, Fut>(mut f: F) -> PendingHandle<P>
     where
         P: Send + 'static,
         F: FnMut(P) -> Fut + Send + 'static,
@@ -77,61 +85,120 @@ impl Stage {
         tokio::spawn(async move {
             println!("running async block");
 
-            // Create a pid with a message sender and a process-exit sender
-            let (sender, mut receiver) = tokio::sync::mpsc::channel::<Msg<P>>(1);
-            let (exit_sender, exit_receiver) = tokio::sync::oneshot::channel::<Exit>();
-            pid_sender.send(Pid{ sender }).ok();
+            // Create a pid with process and system message senders
+            let (sender, mut receiver) = tokio::sync::mpsc::channel::<ProcessMsg<P>>(1);
+            let (sys_sender, mut sys_receiver) = tokio::sync::mpsc::channel::<SystemMsg>(1);
+            let pid = Pid{ sender, sys_sender };
 
+            // Send the pid back to the PendingPid so it can be upgraded to a Pid
+            pid_sender.send(pid).ok();
             println!("sent pid");
 
-            while let Some(msg) = receiver.recv().await {
-                println!("received msg");
+            // Processes linked to receive exit messages
+            let mut links: Vec<UntypedPid> = Vec::new();
+            let mut exit = Exit::Continue;
 
+            // Process incoming ProcessMsg, exiting the loop if the process definition
+            // function returns an exit, or if a linked process exits abnormally
+            loop {
+                println!("waiting for msg");
+
+                let msg = tokio::select! {
+                    Some(msg) = receiver.recv() => msg,
+                    Some(msg) = sys_receiver.recv() => ProcessMsg::System(msg),
+                };
+
+                println!("received msg");
                 match msg {
-                    Msg::Actor(actor_msg) => {
-                        let exit = f(actor_msg).await;
+                    ProcessMsg::Process(process_msg) => {
+                        // Delegate processing to process function
+                        let exit = f(process_msg).await;
+
+                        // Exit if the process requests
                         if exit != Exit::Continue {
-                            println!("sending exit");
-                            exit_sender.send(exit).ok();
                             break;
                         }        
                     },
-                    Msg::LinkExit(exit) => {
-                        println!("received exit {:?} from link", exit);
-                        if exit == Exit::Error {
-                            println!("sending exit received from link");
-                            exit_sender.send(exit).ok();
-                            break;
-                        } else {
-                            println!("ignoring exit received from link");
+                    ProcessMsg::System(system_msg) => {
+                        match system_msg {
+                            // Link a process
+                            SystemMsg::Link(pid) => {
+                                links.push(pid);
+                            },
+
+                            // Unlink a process
+                            SystemMsg::Unlink(pid) => {
+                                // TODO: Fix this... unable to do the following because tokio::mpsc::Sender
+                                // does not implement PartialEq.
+                                // links.retain(|&x| x != pid);
+
+                            },
+
+                            // Exit if linked process exits abnormally
+                            SystemMsg::LinkExit(linked_exit) => {
+                                exit = linked_exit;
+                                break;
+                            },
                         }
                     },
+                }
+            }
+            println!("loop finished");
+
+            if exit != Exit::Normal {
+                for link in links.iter() {
+                    link.send(SystemMsg::LinkExit(exit)).await;
                 }
             }
 
             println!("async block exiting");
         });
 
-        PendingPid{ receiver: pid_receiver }
+        // Send PendingPid to the caller
+        PendingHandle{ receiver: pid_receiver }
     }
 }
 
-pub struct PendingPid<P: Actor> {
+pub struct PendingHandle<P: Process> {
     receiver: tokio::sync::oneshot::Receiver<Pid<P>>,
 }
 
-impl<P: Actor> PendingPid<P> {
+impl<P: Process> PendingHandle<P> {
     pub async fn pid(self) -> Pid<P> {
         self.receiver.await.unwrap()
     }
 }
 
-pub struct Pid<P: Actor> {
-    sender: tokio::sync::mpsc::Sender<Msg<P>>,
+pub struct Pid<P: Process> {
+    sender: tokio::sync::mpsc::Sender<ProcessMsg<P>>,
+    sys_sender: tokio::sync::mpsc::Sender<SystemMsg>,
 }
 
-impl<P: Actor> Pid<P> {
+impl<P: Process> Pid<P> {
     pub async fn send(&self, msg: P) {
-        self.sender.send(Msg::Actor(msg)).await.ok();
+        self.sender.send(ProcessMsg::Process(msg)).await.ok();
+    }
+
+    fn untyped(&self) -> UntypedPid {
+        UntypedPid { sender: self.sys_sender.clone() }
+    }
+}
+
+/// A Pid which can only send system messages
+#[derive(Clone)]
+pub struct UntypedPid {
+    sender: tokio::sync::mpsc::Sender<SystemMsg>,
+}
+
+impl UntypedPid {
+    pub async fn send(&self, msg: SystemMsg) {
+        self.sender.send(msg).await.ok();
+    }
+
+    pub async fn link(&self, other_pid: &UntypedPid) {
+        let (_self_link, _other_link) = tokio::join! {
+            self.send(SystemMsg::Link(other_pid.clone())),
+            other_pid.send(SystemMsg::Link(self.clone())),
+        };
     }
 }
